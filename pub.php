681aea84e2350a93e3f256bc19e2f3309e005970
akumo <akumo@yahoo.com>
2017-09-02 15:01:47 +0700

    Added sample file.

---
+++ pub.php
@@ -0,0 +1,48 @@
+<?php
+ 
+
+  function pubMqtt($topic,$msg){
+       
+      put("https://api.netpie.io/topic/PIE/$topic?retain",$msg);
+ 
+  }
+  function getMqttfromlineMsg($lineMsg){
+ 
+    $pos = strpos($lineMsg, ":");
+    if($pos){
+      $splitMsg = explode(":", $lineMsg);
+      $topic = $splitMsg[0];
+      $msg = $splitMsg[1];
+      pubMqtt($topic,$msg);
+    }else{
+      $topic = "raw";
+      $msg = $lineMsg;
+      pubMqtt($topic,$msg);
+    }
+  }
+ 
+function put($url,$tmsg)
+{
+      
+    $ch = curl_init($url);
+ 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+     
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+     
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+ 
+    curl_setopt($ch, CURLOPT_USERPWD, "auaHe5TvVQz6Sv2:wVIHkenZhYOC3kmlEvLi9BfLQ");
+     
+    $response = curl_exec($ch);
+     
+    curl_close ($ch);
+     
+    return $response;
+}
+ 
+?>
